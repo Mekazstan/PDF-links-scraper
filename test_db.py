63a373df8f04fd05b4316dedd07b381ea645ad2f
Mekazstan <mekastans@gmail.com>
2023-10-17 10:41:44 +0100

    Updated DB test script

--- test_db.py
+++ test_db.py
@@ -1,4 +1,5 @@
 import sqlite3
+import csv
 
 # Connect to the SQLite database
 conn = sqlite3.connect('pdf_data.db')
@@ -10,10 +11,21 @@ cursor.execute('SELECT * FROM pdf_data')
 # Fetch all rows from the executed query
 rows = cursor.fetchall()
 
-# You can iterate through the rows to access the data
-for row in rows:
-    # The row variable contains the data from each row
-    print(row)
+# Define the path to the CSV file
+csv_file_path = 'pdf_data.csv'
+
+# Open the CSV file for writing
+with open(csv_file_path, 'w', newline='', encoding='utf-8') as csv_file:
+    csv_writer = csv.writer(csv_file)
+
+    # Write the header row based on your table structure (e.g., id, title, pdf_link)
+    csv_writer.writerow(['id', 'title', 'pdf_link'])
+
+    # Write data rows
+    csv_writer.writerows(rows)
+
+print(f'Data has been exported to {csv_file_path}')
 
 # Close the database connection when you're done
 conn.close()
+
